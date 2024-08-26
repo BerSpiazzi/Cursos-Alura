4443e810a32a1769a1cc1dd3b28b26394a1e07ff
Bernardo Bortot Spiazzi <ber.spiazzidv8@gmail.com>
2024-08-25 22:07:12 -0300

    Aula 3. Documentação da API

---
+++ Java e Spring Boot/src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,26 @@
+package med.voll.api.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components().addSecuritySchemes(
+                                "bearer-key",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(io.swagger.v3.oas.models.security.SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                        )
+                );
+    }
+
+}
